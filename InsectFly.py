13ab4bfaaf2ed763c3631836b5db0ce6b58096ef
Lauren Curtis <lauren_curtis1@baylor.edu>
2021-09-13 17:17:19 -0500

    Creation

---
+++ InsectFly.py
@@ -0,0 +1,10 @@
+import InsectClass as i
+
+my_fly = i.Insect()
+my_fly.fly()
+print("My fly flew: ", my_fly.get_length(), " miles")
+
+my_mosquito = i.Insect()
+my_mosquito.fly()
+print("My mosquito flew: ",my_mosquito.get_length," miles")
+
