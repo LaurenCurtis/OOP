f09a600cffdae29232db6d660abf06d5bb683d87
Lauren Curtis <lauren_curtis1@baylor.edu>
2021-09-15 16:12:06 -0500

    update

--- InsectFly.py
+++ InsectFly.py
@@ -1,10 +1,10 @@
 import InsectClass as i
 
-my_fly = i.Insect()
+my_fly = i.Insect(2,4)
 my_fly.fly()
 print("My fly flew: ", my_fly.get_length(), " miles")
 
-my_mosquito = i.Insect()
+my_mosquito = i.Insect(4,2)
 my_mosquito.fly()
 print("My mosquito flew: ",my_mosquito.get_length()," miles")
 
