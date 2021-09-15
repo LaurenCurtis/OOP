a970c5fefaa806b2fd11052831f99b450514d478
Lauren Curtis <lauren_curtis1@baylor.edu>
2021-09-15 16:43:55 -0500

    addition

--- CellPhoneProgram.py
+++ CellPhoneProgram.py
@@ -1,6 +1,11 @@
 import CellPhoneClass as c 
 
-ipohone = c.CellPhone('Apple', 12, '1,000')
+ipohone = c.CellPhone('Apple', '12', '1,000')
 print(ipohone.get_manufact())
 print(ipohone.get_model())
 print(ipohone.get_retail_price())
+
+samsung = c.CellPhone('Samsung', 'Galaxy', '800' )
+print(samsung.get_manufact())
+print(samsung.get_model())
+print(samsung.get_retail_price())
