d1d856f1f2188b31bef182e5a769b6abeb2172ab
Lauren Curtis <lauren_curtis1@baylor.edu>
2021-09-15 16:16:17 -0500

    hide

--- CoinClass.py
+++ CoinClass.py
@@ -10,7 +10,7 @@ class Coin:
 #def_init is defining all attributes
 
     def __init__(self):
-        self.sideup = 'Heads'
+        self.__sideup = 'Heads'
 
     # The toss method generates a random number
     # in the range of 0 through 1. If the number
@@ -19,12 +19,12 @@ class Coin:
 
     def toss(self):
         if random.randint(0, 1) == 0:
-            self.sideup = 'Heads'
+            self.__sideup = 'Heads'
         else:
-            self.sideup = 'Tails'
+            self.__sideup = 'Tails'
 
     # The get_sideup method returns the value
     # referenced by sideup.
 
     def get_sideup(self):
-            return self.sideup
+            return self.__sideup
