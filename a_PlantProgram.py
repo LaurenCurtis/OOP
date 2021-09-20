5b50465bb8617dfcb87f8c509b86c72abc3ef1ca
Lauren Curtis <lauren_curtis1@baylor.edu>
2021-09-20 16:59:36 -0500

    flower

--- a_PlantProgram.py
+++ a_PlantProgram.py
@@ -2,12 +2,9 @@ import a_PlantClass as pc
 
 primrose = pc.Plant("Green")
 
-sunflower = pc.Flower("Yellow")
+sunflower = pc.Flower("Yellow", 10)
 
 print(primrose.get_color())
 
 print(sunflower.get_color())
 print(sunflower.get_petals())
-
-
-print(primrose.get_petals())
